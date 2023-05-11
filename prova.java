21e9a142eaabc9ec8b78c0f7d5174c51e2bad67f
Hollow126 <tizigess@gmail.com>
2023-05-11 11:33:05 +0200

    modificato file java prova 1  e aggiunto file prova2

--- prova.java
+++ prova.java
@@ -2,6 +2,7 @@
 
 public class prova {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
+        System.out.println("Ciao");
     }
 }
---
+++ prova2.java
@@ -0,0 +1,9 @@
+//package java-FullStack-Assignement;
+
+public class prova2 {
+    public static void main(String[] args) 
+    {
+        int a = 1;
+        System.out.println(a);       
+    }
+}
