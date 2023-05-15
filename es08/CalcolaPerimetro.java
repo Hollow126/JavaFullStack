a5495952322a5c7a86868cc53d30eea09ec438a8
Hollow126 <tizigess@gmail.com>
2023-05-15 12:35:20 +0200

    adesso il programma chiede il nome al utente

--- es08/CalcolaPerimetro.java
+++ es08/CalcolaPerimetro.java
@@ -7,26 +7,28 @@ public class CalcolaPerimetro
     public static void main(String[] args) 
     {
         Scanner tastiera = new Scanner(System.in);           // creazione oggetto di classe Scanner di nome Tastiera
-        boolean eInserito=false;
-        while (eInserito == false) 
+        boolean numInseritoCorrettamente=false;
+        while (numInseritoCorrettamente == false) 
         {
             try                                                 
-            {        
+            {       
+                System.out.println("inserisci il tuo nome");     
+                String nome =tastiera.nextLine();
                 System.out.println("inserisci la base");     
                 double base = Double.parseDouble(tastiera.nextLine());
                 System.out.println("scrivi la altezza");
                 double altezza = Double.parseDouble(tastiera.nextLine());
                 if (base >= 40 || altezza >= 40)
                 {
-                    System.out.println("numero troppo grande (maggiore di 40), riprova con un numero più piccolo ");
-                    eInserito=false;
+                    System.out.println(" ehy " + nome + " il numero è troppo grande (maggiore di 40), riprova con un numero più piccolo ");
+                    numInseritoCorrettamente=false;
                 }
                 else
                 {
                     double area = base*altezza;
                     double perimetro = base*2 + altezza*2;
-                    System.out.println("l'area è " + area + ", il perimetro è " + perimetro);
-                    eInserito=true;
+                    System.out.println("ciao "+ nome + " l'area è " + area + ", il perimetro è " + perimetro);
+                    numInseritoCorrettamente=true;
                 }
             } 
             catch (InputMismatchException e) 
