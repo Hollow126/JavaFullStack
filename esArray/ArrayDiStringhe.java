0d4409b993c6649394ce5dbfae86b6b3ba458b9f
Hollow126 <tizigess@gmail.com>
2023-05-17 12:17:51 +0200

    creato nuovo branch

---
+++ esArray/ArrayDiStringhe.java
@@ -0,0 +1,30 @@
+package esArray;
+
+import java.util.Random;
+
+public class ArrayDiStringhe 
+{
+    public static void main(String[] args) 
+    {
+        Random rand = new Random(); 
+        int limite = 41; 
+        int i=0;
+        String[] frasi = new String [5];
+        for (i=0;i<5;i++) 
+        {
+            frasi[i] = Integer.toString(rand.nextInt(limite));
+            System.out.println(frasi[i]);
+            if (i==(frasi.length-1))
+            {
+                System.out.println("fineArray");
+            }
+            
+        }
+        for (String frase : frasi) 
+        {  
+        System.out.println(frase);
+        }
+    }
+    /*fai in modo che l'utente riempa l'array con le frasi che immette lui anziche di usare numeri random*/
+}
+ 
---
+++ esArray/NumerazioneArray.java
@@ -0,0 +1,44 @@
+package esArray;
+
+import java.util.*;
+
+
+public class NumerazioneArray 
+{
+    public static void main(String[] args) 
+    {
+        Scanner tastiera = new Scanner(System.in);
+        Random rand = new Random(); 
+        int upperbound = 14; 
+        int[] exampleArray = {1,2,3,4,5};
+        // System.out.println("scrivi quanto grande vuoi l'array");
+        // int dimensione = tastiera.nextInt();
+        // int [] array = new int[dimensione];
+        // for (int i=0;i<array.length;i++) 
+        // {
+        //     if (i == (array.length-1)) 
+        //     {
+        //         System.out.println(array[i]);
+        //     }
+        //     else
+        //     {
+        //         array[i] = rand.nextInt(upperbound);
+        //         System.out.println(array[i]);
+        //     }
+        // }
+        for(int i=0;i<=exampleArray.length;i++)
+        {
+      
+            if (i == (exampleArray.length)) 
+            {
+                System.out.println("fine array");
+            }
+            else
+            {
+                //exampleArray[i] = rand.nextInt(upperbound);
+                System.out.println(exampleArray[i]);
+            }
+        }
+        tastiera.close();
+    }    
+}
